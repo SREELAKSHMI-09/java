23a4eb244986390ba22be33bbf74cdc3f068d153
SREELAKSHMI-09 <117641272+SREELAKSHMI-09@users.noreply.github.com>
2023-03-28 14:10:32 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,8 @@
+ class demo {
+  public static void main(String[] args) {
+  System.out.println("Sreelakshmi M - 22MCA053 - Object Oriented Programming Lab(20MCA132) - 24/03/2023");
+    System.out.println("Hello World");
+  }
+}
+
+
